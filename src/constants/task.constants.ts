365014f84d5a9b151fa473fdeda650f02a03c9cb
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-09-13 18:54:19 -0300

    :sparkles: Feat.: Adding difficult level and status in Tasks

---
+++ src/constants/task.constants.ts
@@ -0,0 +1,13 @@
+export enum TaskType {
+    DIARIA = 'Diária',
+    SEMANAL = 'Semanal',
+    MENSAL = 'Mensal',
+    EMERGENTE = 'Emergente',
+};
+
+export const difficultyMap: Record<TaskType, string> = {
+    [TaskType.DIARIA]: "5",
+    [TaskType.SEMANAL]: "10",
+    [TaskType.MENSAL]: "20",
+    [TaskType.EMERGENTE]: "40",
+};
--- src/modules/tasks/services/tasks.service.ts
+++ src/modules/tasks/services/tasks.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Task } from "../entities/tasks.entity";
 import { PrismaService } from "src/prisma/prisma.service";
+import { difficultyMap } from "src/constants/task.constants";
 
 @Injectable()
 export class TasksService {
@@ -15,11 +16,18 @@ export class TasksService {
     }
 
     async create(task: Task): Promise<Task> {
-        return await this.prisma.tasks.create({ data: task });
+        const difficultUpdated: string = difficultyMap[task.type] || "0";
+
+        return await this.prisma.tasks.create({
+            data: {
+                ...task,
+                difficult: difficultUpdated,
+                status: "Pendente"
+            }
+        });
     }
 
     async update(task: Task): Promise<Task> {
-
         let taskSearched = await this.findById(task.id);
 
         if (!taskSearched || !task.id)
@@ -35,8 +43,12 @@ export class TasksService {
         let taskSearched = await this.findById(taskId);
 
         if (!taskSearched || !taskId)
-            throw new HttpException("Tarefa não existente!", HttpStatus.NOT_FOUND);
+            throw new HttpException("Tarefa não existe!", HttpStatus.NOT_FOUND);
 
         await this.prisma.tasks.delete({ where: { id: taskId } });
+    }
+
+    async completionTask(taskId: string) {
+
     }
 }
