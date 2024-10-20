73f6b29f7b7bbf22120d8b79c4b84067e0738f95
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-10-20 14:55:08 -0300

    :broom: cleanup: removing comments block

--- src/modules/tasks/constants/task.constants.ts
+++ src/modules/tasks/constants/task.constants.ts
@@ -6,17 +6,3 @@ export const difficultyMap = {
 }
 
 export const validTypes = ['Diária', 'Semanal', 'Mensal', 'Emergente'];
-
-// export enum TaskType {
-//     DIARIA = 'Diária',
-//     SEMANAL = 'Semanal',
-//     MENSAL = 'Mensal',
-//     EMERGENTE = 'Emergente',
-// };
-
-// export const difficultyMap: Record<TaskType, number> = {
-//     [TaskType.DIARIA]: 5,
-//     [TaskType.SEMANAL]: 10,
-//     [TaskType.MENSAL]: 20,
-//     [TaskType.EMERGENTE]: 40,
-// };
